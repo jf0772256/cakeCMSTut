0d14dc5d5add05034f947726ec4a979ef67952ee
Jesse Fender <jessefender@jessefender.com>
2024-02-12 14:25:18 -0600

    adding slug generation, not sure it

--- src/Model/Table/ArticlesTable.php
+++ src/Model/Table/ArticlesTable.php
@@ -3,6 +3,8 @@
 	namespace App\Model\Table;
 
     use Cake\ORM\Table;
+    use Cake\Utility\Text;
+    use Cake\Event\EventInterface;
 
     class ArticlesTable extends Table
 	{
@@ -10,5 +12,14 @@
         {
             parent::initialize($config);
             $this->addBehavior('Timestamp');
+        }
+
+        public function beforeSave(EventInterface $event, $entity, $options)
+        {
+            if ($entity->isNew() && empty($entity->slug))
+            {
+                $sluggedTitle = Text::slug($entity->title);
+                $entity->slug = substr($sluggedTitle, 0, 191);
+            }
         }
 	}
