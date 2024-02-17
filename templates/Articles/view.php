48e9e68e496c9a6f7c403a17f936bbeaca353a4c
Jesse Fender <jessefender@jessefender.com>
2024-02-17 11:26:49 -0600

    Added delete link to the view template using the postLink method had to change the p tag to a div to make it work inline but it works as expected

--- templates/Articles/view.php
+++ templates/Articles/view.php
@@ -11,6 +11,10 @@
 <p>
     <small>Created: <?= $article->created->format(DATE_RFC850) ?></small>
 </p>
-<p>
-    <?= $this->Html->link('Back', ['action' => 'index']); ?> <span style="margin: 0 0.5rem;"></span> <?= $this->Html->link('Edit', ['action' => 'edit', $article->slug]) ?>
-</p>
+<div>
+    <?= $this->Html->link('Back', ['action' => 'index']); ?>
+    <span style="margin: 0 0.5rem;"></span>
+    <?= $this->Html->link('Edit', ['action' => 'edit', $article->slug]) ?>
+    <span style="margin: 0 0.5rem;"></span>
+    <?= $this->Form->postLink('Delete', ['action' => 'delete', $article->slug], ['confirm' => 'Are you sure?']); ?>
+</div>
