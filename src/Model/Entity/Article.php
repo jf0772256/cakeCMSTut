5d6065b5d86b7bbc8247e0066c1137336d674f24
Jesse Fender <jessefender@jessefender.com>
2024-02-17 08:29:21 -0600

    Creating Entity class definition by hand for an Article

---
+++ src/Model/Entity/Article.php
@@ -0,0 +1,17 @@
+<?php
+
+	namespace App\Model\Entity;
+
+	use Cake\ORM\Entity;
+
+	class Article extends Entity
+	{
+        protected array $_accessible = [
+            'title' => true,
+            'body' => true,
+            'published' => true,
+            'created' => true,
+            'modified' => true,
+            'users' => true
+        ];
+	}
