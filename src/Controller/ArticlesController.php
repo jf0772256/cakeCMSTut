d38165fcab4b41be8aa5f0094658d673f74d3171
Jesse Fender <jessefender@jessefender.com>
2024-02-17 10:06:29 -0600

    Adding add method on ArticlesController

--- src/Controller/ArticlesController.php
+++ src/Controller/ArticlesController.php
@@ -17,4 +17,24 @@
             $article = $this->Articles->findBySlug($slug)->firstOrFail();
             $this->set(compact('article'));
         }
+
+        public function add()
+        {
+            $article = $this->Articles->newEmptyEntity();
+            if ($this->request->is('post'))
+            {
+                $article = $this->Articles->patchEntity($article, $this->request->getData());
+                // setting this manually to 1 because we dont have the authentication methods in-place yet
+                $article->user = 1;
+                // we will change that later
+
+                if ($this->Articles->save($article))
+                {
+                    $this->Flash->success(__('Article created successfully.'));
+                    return $this->redirect(['action' => 'index']);
+                }
+                $this->Flash->error(__('Unable to save the article.'));
+            }
+            $this->set('article', $article);
+        }
 	}
