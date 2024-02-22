60340ea4c1ed0269f7eb5bd221dd8fabeff58786
Jesse Fender <jessefender@jessefender.com>
2024-02-22 15:19:44 -0600

    adding tags form control to the add and edit view template

--- templates/Articles/add.php
+++ templates/Articles/add.php
@@ -9,6 +9,7 @@
     echo $this->Form->control('user_id', ['type'=>'hidden', 'value' => 1]);
     echo $this->Form->control('title');
     echo $this->Form->control('body', ['rows' => 5]);
+    echo $this->Form->control('tags._ids', ['options' => $tags]);
     echo $this->Form->button(__('Submit'));
     echo $this->Form->end();
 ?>
